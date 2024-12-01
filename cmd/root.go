dd4e3e970aa20f4f0f7e2f27541bcedf4f7a9014
Pablo Seminario <pablo@seminar.io>
2024-12-01 10:13:00 +0100

    Bump version to 0.1.8

--- cmd/root.go
+++ cmd/root.go
@@ -20,7 +20,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const version = "0.1.7"
+const version = "0.1.8"
 const apiEndpoint = "https://dashboard.securaebackup.com/api/v1"
 
 const flagApiToken = "api-token"
